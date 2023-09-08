72fc5bab7f3337abcc335c4670973be88e96fdb9
wesll <wesll@DESKTOP-3ERCKC4>
2023-09-08 18:53:42 -0300

    Add project files.

---
+++ src/DDD.Api/Controllers/HealthCheckController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DDD.Api.Controllers;
+
+[Produces("application/json")]
+[ApiController]
+[ApiVersion("1.0")]
+[Route("api/[controller]")]
+public class HealthCheckController : ControllerBase
+{
+    private readonly ILogger<HealthCheckController> _logger;
+
+    public HealthCheckController(ILogger<HealthCheckController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet]
+    public IActionResult Get()
+    {
+        _logger.LogInformation("Health check");
+        return Ok("Healthy");
+    }
+}
---
+++ tests/DDD.Tests/Api/Controllers/HealthCheckControllerTests.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using System.Net;
+using System.Text.Json;
+using Xunit;
+
+namespace DDD.Tests.Api.Controllers;
+
+public class HealthCheckControllerIntegrationTests : IClassFixture<WebApplicationFactory<Program>>
+{
+    private readonly HttpClient _client;
+
+    public HealthCheckControllerIntegrationTests(WebApplicationFactory<Program> factory)
+    {
+        _client = factory.CreateClient();
+    }
+
+    [Fact]
+    public async Task HealthCheck_ReturnsHealthyStatus()
+    {
+        // Arrange
+        var requestUri = "api/healthcheck";
+
+        // Act
+        var response = await _client.GetAsync(requestUri);
+
+        // Assert
+        Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        var content = await response.Content.ReadAsStringAsync();
+        var deserializedContent = JsonSerializer.Deserialize<string>(content);
+        Assert.Equal("Healthy", deserializedContent);
+    }
+}
