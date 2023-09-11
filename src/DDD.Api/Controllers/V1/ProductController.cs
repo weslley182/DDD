982d256d9906c0a82140c9ae3556262ed53a71a5
wesley machado <wesleymachadotoo@tooseguros.com.br>
2023-09-10 22:12:42 -0300

    new test

--- src/DDD.Api/Controllers/V1/ProductController.cs
+++ src/DDD.Api/Controllers/V1/ProductController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DDD.Application.UseCase.Interface;
+using Microsoft.AspNetCore.Mvc;
 
 namespace DDD.Api.Controllers.V1;
 
@@ -8,10 +9,16 @@ namespace DDD.Api.Controllers.V1;
 [Route("api/v{version:apiVersion}/[controller]")]
 public class ProductController : ControllerBase
 {
+    private readonly IProductUseCase _useCase;
+    public ProductController(IProductUseCase useCase)
+    {
+        _useCase = useCase;
+    }
 
     [HttpGet]
     public IActionResult Get()
     {
+        _useCase.GetAllProductsAsyc();
         return Ok("GetProduct");
     }
 }
